0bad2262dec7f5d7a18ac8d8cd9cf52317f8b84d
matthlud <42676592+matthlud@users.noreply.github.com>
2023-11-19 13:38:34 +0100

    [info] repo clean-up

--- src/runner.py
+++ src/runner.py
@@ -1,7 +1,7 @@
 from file_handler import FileHandler
 from player import Player
 
-sound_file = FileHandler(r"D:\Projekte\Coding\soundfile_analyzer", "You_Can_Do_It.mp3")
+sound_file = FileHandler(r"D:\Projekte\Coding\soundfile_analyzer\tests", "You_Can_Do_It.mp3")
 sound_file.printFiles()
 
 player = Player(sound_file.path_filename)
