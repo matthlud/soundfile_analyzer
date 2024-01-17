bb4aec78f075702bc02ae3386b9bd4eb54e9f18d
matthlud <42676592+matthlud@users.noreply.github.com>
2024-01-17 21:11:23 +0100

    [info] Change commenting

--- src/runner.py
+++ src/runner.py
@@ -7,9 +7,11 @@ from analyzer import Analyzer
 sound_file = FileHandler(r"D:\Projekte\Coding\soundfile_analyzer\tests", "You_Can_Do_It.wav")
 sound_file.print_files()
 
-audio_info = Analyzer(sound_file.path_filename)
-audio_info.print_meta_info()
-audio_info.visualize_spectrogram()
+# audio_info = Analyzer(sound_file.path_filename)
+# audio_info.print_meta_info()
+# audio_info.visualize_waveform()
+# audio_info.visualize_spectrogram()
 
-# player = Player(sound_file.path_filename)
-# player.play_forward()
+
+player = Player(sound_file.path_filename)
+player.play_forward()
