fc5e0588dce34a2a035b662b2b4dba497054a7d3
matthlud <42676592+matthlud@users.noreply.github.com>
2024-05-11 23:45:08 +0200

    Add hardware information to runner script

--- src/runner.py
+++ src/runner.py
@@ -1,17 +1,20 @@
 """module docstring
 """
 from file_handler import FileHandler
+from hardware_info import HardwareInfo
 from player import Player
 from analyzer import Analyzer
 
 sound_file = FileHandler(r"D:\Projekte\Coding\soundfile_analyzer\tests", "You_Can_Do_It.wav")
 sound_file.print_files()
 
+info = HardwareInfo()
+info.display_info()
+
 audio_info = Analyzer(sound_file.path_filename)
 audio_info.print_meta_info()
 # audio_info.visualize_waveform()
 # audio_info.visualize_spectrogram()
-
 
 player = Player(sound_file.path_filename)
 player.play_forward()
