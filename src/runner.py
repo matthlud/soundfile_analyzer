c0ce0b46d179e7871ed37bce2136f6cffd5f8168
matthlud <42676592+matthlud@users.noreply.github.com>
2024-05-11 23:26:09 +0200

    Uncomment analyzer functions

--- src/runner.py
+++ src/runner.py
@@ -7,8 +7,8 @@ from analyzer import Analyzer
 sound_file = FileHandler(r"D:\Projekte\Coding\soundfile_analyzer\tests", "You_Can_Do_It.wav")
 sound_file.print_files()
 
-# audio_info = Analyzer(sound_file.path_filename)
-# audio_info.print_meta_info()
+audio_info = Analyzer(sound_file.path_filename)
+audio_info.print_meta_info()
 # audio_info.visualize_waveform()
 # audio_info.visualize_spectrogram()
 
